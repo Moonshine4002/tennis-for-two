3f6d16cae9908eebb8c5acccc065e17c7ce01bd5
Moonshine4002 <moonshine2004@163.com>
2025-03-18 17:42:15 +0800

    add skill

--- src/brick/main.gd
+++ src/brick/main.gd
@@ -11,6 +11,12 @@ signal exit(node: Node)
 var level := 1
 
 var exit_flag := false
+var prolong_flag := false
+
+var items := {
+	"proliferation": 10,
+	"prolong": 10,
+}
 
 
 func _ready() -> void:
@@ -41,6 +47,14 @@ func _process(_delta: float) -> void:
 				ball_flag = true
 		if not ball_flag:
 			level_over.emit(false)
+
+	var items_index := 0
+	for value in items.values():
+		var text: String = $ItemList.get_item_text(items_index)
+		text = text.rstrip("0123456789")
+		text += str(value)
+		$ItemList.set_item_text(items_index, text)
+		items_index += 1
 
 
 func select_level(lv: int) -> void:
@@ -88,6 +102,8 @@ func _on_bottom_area_body_entered(ball: Node2D) -> void:
 	if ball is not BrickBall:
 		return
 	ball.queue_free()
+	if randf() < 0.05:
+		items[items.keys()[randi_range(0, items.size() - 1)]] += 1
 	#call_deferred("add_ball")
 
 
@@ -97,3 +113,56 @@ func _on_level_over(win: bool) -> void:
 	else:
 		$Info.text = "you lose!"
 	exit_flag = true
+
+
+func _on_item_list_item_clicked(index: int, _at_position: Vector2, mouse_button_index: int) -> void:
+	if mouse_button_index == MOUSE_BUTTON_LEFT:
+		return
+	match index:
+		0:
+			proliferation()
+		1:
+			prolong()
+
+
+func proliferation() -> void:
+	if items["proliferation"] <= 0:
+		return
+	items["proliferation"] -= 1
+	for child in get_children():
+		if child is not BrickBall:
+			continue
+		var ball: BrickBall = ball_scene.instantiate()
+		ball.position = child.position
+		ball.set_linear_velocity(Vector2.RIGHT.rotated(randf_range(0, 2 * PI)))
+		add_child(ball)
+
+
+func prolong() -> void:
+	if prolong_flag:
+		return
+	if items["prolong"] <= 0:
+		return
+	items["prolong"] -= 1
+	prolong_flag = true
+	$Platform/Sprite2D.scale.x = 2
+	$Platform/CollisionShape2D.scale.x = 2
+	$Platform/CollisionShape2D.scale.y = 2
+	$Platform/CollisionShape2D.position.y = 50
+	var timer = Timer.new()
+	add_child(timer)
+	timer.start(5)
+	await timer.timeout
+	timer.queue_free()
+	$Platform/Sprite2D.scale.x = 1
+	$Platform/CollisionShape2D.scale.x = 1
+	$Platform/CollisionShape2D.scale.y = 1
+	$Platform/CollisionShape2D.position.y = 29
+	prolong_flag = false
+
+
+func _unhandled_key_input(event: InputEvent) -> void:
+	if event.is_action_pressed("num1"):
+		proliferation()
+	elif event.is_action_pressed("num2"):
+		prolong()
