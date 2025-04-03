2abc03f72ee5c82c78ec66293ab854743a415af0
Moonshine4002 <moonshine2004@163.com>
2025-04-03 08:54:36 +0800

    add lobby list credits: https://godotengine.org/asset-library/asset/2797

--- src/ui.gd
+++ src/ui.gd
@@ -3,6 +3,8 @@ extends Control
 
 func _ready() -> void:
 	state_disabled()
+	Lobby.player_connected.connect(_on_lobby_player_connected)
+	Lobby.player_disconnected.connect(_on_lobby_player_disconnected)
 
 
 @rpc("any_peer", "call_local", "reliable")
@@ -36,7 +38,7 @@ func _on_join_pressed() -> void:
 	state_linking()
 
 
-func _on_load_pressed() -> void:
+func _on_start_pressed() -> void:
 	if multiplayer.is_server():
 		state_disabled.rpc()
 		Lobby.load_game.rpc("res://src/game.tscn")
@@ -46,3 +48,25 @@ func _on_exit_pressed() -> void:
 	state_disabled()
 	get_tree().change_scene_to_file("res://main.tscn")
 	Lobby.remove_multiplayer_peer()
+
+
+func _on_lobby_player_connected(peer_id, player_info) -> void:
+	refresh_lobby()
+
+
+func _on_lobby_player_disconnected(peer_id) -> void:
+	refresh_lobby()
+
+
+func refresh_lobby():
+	var players: Dictionary = Lobby.players
+	#players.sort()
+	$Link/List.clear()
+
+	for id in players:
+		var list_text: String = players[id]["name"]
+		if id == multiplayer.get_unique_id():
+			list_text += " (You)"
+		$Link/List.add_item(list_text)
+
+	$Link/Start.disabled = not multiplayer.is_server()
